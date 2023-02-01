be25dadde65f3a420d4c45031cbb7415a0e1e2df
K.YASASWINI <84077636+yasaswini-k@users.noreply.github.com>
2023-02-01 07:56:19 +0530

    Create client.py

---
+++ TCP-IP/client.py
@@ -0,0 +1,13 @@
+from socket import *
+serverName = '127.0.0.1'
+serverPort = 12000
+
+2
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence = input("\nEnter file name: ")
+clientSocket.send(sentence.encode())
+filecontents = clientSocket.recv(1024).decode()
+print ('\nFrom Server:\n')
+print(filecontents)
+clientSocket.close()
