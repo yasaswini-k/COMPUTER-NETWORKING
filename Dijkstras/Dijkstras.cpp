2cee6cb53740205c17a7104e7697ad2c5df1d869
K.YASASWINI <84077636+yasaswini-k@users.noreply.github.com>
2023-02-02 08:41:45 +0530

    Update Dijkstras.cpp

--- Dijkstras/Dijkstras.cpp
+++ Dijkstras/Dijkstras.cpp
@@ -5,13 +5,10 @@
 #define V 4
 int minDistance(int dist[], bool sptSet[])
 {
-
     int min = INT_MAX, min_index;
-
     for (int v = 0; v < V; v++)
         if (sptSet[v] == false && dist[v] <= min)
             min = dist[v], min_index = v;
-
     return min_index;
 }
 void printSolution(int dist[])
